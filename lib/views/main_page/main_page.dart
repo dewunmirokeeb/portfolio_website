b2f18ec82179e100c3a6fabd59e587150db7aa46
dewunmirokeeb <adewunmirokeeb6@gmail.com>
2023-10-29 23:10:04 +0100

    update

--- lib/views/main_page/main_page.dart
+++ lib/views/main_page/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rokeebfolio/components/colors.dart';
 import 'package:rokeebfolio/components/custom_widgets/custom_app_bar.dart';
 import 'package:rokeebfolio/components/custom_widgets/styled_container.dart';
 
@@ -14,15 +15,17 @@ final ScrollController _scrollController = ScrollController();
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      controller: _scrollController,
-      physics: const BouncingScrollPhysics(),
-      children: const [
-        CustomAppBar(),
-        SizedBox(height: 50),
-        HomePage(),
-        SizedBox(height: 1000),
-      ],
+    return Scaffold(
+      body: ListView(
+        controller: _scrollController,
+        physics: const BouncingScrollPhysics(),
+        children: const [
+          CustomAppBar(),
+          SizedBox(height: 100),
+          HomePage(),
+          SizedBox(height: 1000),
+        ],
+      ),
     );
   }
 }
@@ -34,19 +37,67 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Container(
       margin: const EdgeInsets.only(),
-      child: const Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
-            children: [],
-          ),
-          StyledContainer(
+          SizedBox(
+              width: size.width * 0.4,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Heloo...",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        "I'm ",
+                        style: TextStyle(
+                          fontSize: 45,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Rokeeb Adedokun",
+                        style: TextStyle(
+                          fontSize: 38,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.lightPrimary!,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    "A Mobile Application Developer",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    '''I'm a passionate Flutter developer with over 3 years of hands-on experience in creating stunning mobile applications. My journey in the world of Flutter has been an exciting one, filled with endless opportunities to craft intuitive and visually appealing apps. I've spent years working with Flutter, honing my skills in crafting cross-platform mobile applications that are both beautiful and functional. I've successfully built and published apps to both the Google Play Store and Apple App Store, reaching a global audience and receiving valuable user feedback. My quest for knowledge doesn't stop with Flutter. I'm currently on a journey to explore React Native, aiming to broaden my cross-platform development capabilities. Beyond that, I have my sights set on Swift development, eager to delve into the iOS ecosystem and unlock new realms of innovation. I thrive on challenges and am dedicated to delivering top-notch solutions to meet your mobile app development needs. Whether it's creating a seamless user experience or bringing your unique app idea to life, I'm here to make it happen. Feel free to reach out, and let's embark on an exciting development journey together!''',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              )),
+          SizedBox(width: size.width * 0.1),
+          const StyledContainer(
             color: Colors.red,
-            width: 350,
-            height: 550,
+            width: 400,
+            height: 600,
           )
         ],
       ),
